780fd5f57497fa912ead402926f0bc3d878f7639
Paul Gannay <paulgannay@msn.com>
2026-01-12 21:05:42 +0100

    Let atomic exercices parameter be configured from cmd line

--- exercises/11_atomics/exercise/main.cpp
+++ exercises/11_atomics/exercise/main.cpp
@@ -15,20 +15,27 @@ int main(int argc, char *argv[]) {
 
   {
     // Initialize views
-    const int N = 2'000;
-    Kokkos::View<double*> histo("histo", 5);
+		int n = 200;
+		int m = 5;
+		if (argc == 3) {
+			n = std::atoi(argv[1]);
+			m = std::atoi(argv[2]);
+		}
+
+    Kokkos::View<double*> histo("histo", m);
 
     // Create histogram
-    generate_histogram(histo, N);
+    generate_histogram(histo, n);
 
     // Check result
     auto res_ = create_mirror_view(histo);
     Kokkos::deep_copy(res_, histo);
 
+		std::cout << "n: " << n << " m: " << m << std::endl;
     std::cout << "res_: " << (int) res_(0) << std::endl;
 
-    const int nsum = N / histo.size();
-    const int computed_sum = (nsum * (nsum - 1) / 2) * histo.size();
+    const int nsum = n / m + (n%m ? 1:0);
+    const int computed_sum = (nsum * (nsum - 1) / 2) * m;
     std::cout << "Computed sum:" << computed_sum << std::endl;
 
     std::cout << "Difference: " << computed_sum - (int)res_(0) << std::endl;
